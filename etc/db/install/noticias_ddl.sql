5393bd0029af6e5f5041e885c9f927e97d8d6092
Marcos Rachid <marcosrachid@gmail.com>
2017-10-06 00:29:32 -0300

    criação de schema de exemplo para exemplo de conexão de sistema

---
+++ etc/db/install/noticias_ddl.sql
@@ -0,0 +1,6 @@
+create table noticias(
+	id_noticia int not null primary key auto_increment,
+	titulo varchar(100),
+	noticia text,
+	data_criacao timestamp default current_timestamp
+);
---
+++ etc/db/install/noticias_schema_create.sql
@@ -0,0 +1,1 @@
+create database portal_noticias;
