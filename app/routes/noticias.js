db0f4d3aba33f7338bd047ce55c8fc8d3b4e3772
Marcos Rachid <marcosrachid@gmail.com>
2017-10-06 00:56:11 -0300

    Isolamento da conexão do mysql em um arquivo de config

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,13 +1,9 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function(app){
 	app.get('/noticias', function(req, res){
 
-		var mysql = require('mysql');
-		var connection = mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			password: 'root',
-			database: 'portal_noticias'
-		});
+		var connection = dbConnection();
 
 		connection.query('select * from noticias', function(erro, result){
 			res.render("noticias/noticias", {noticias: result});
