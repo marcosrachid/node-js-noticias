e5515e74550e7b189082f0b1d4269b2ccadeca03
Marcos Rachid <marcosrachid@gmail.com>
2017-10-06 00:38:54 -0300

    fazendo uma query usando o driver do mysql importado pelo node

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,19 @@
 module.exports = function(app){
 	app.get('/noticias', function(req, res){
-		res.render("noticias/noticias")
+
+		var mysql = require('mysql');
+		var connection = mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: 'root',
+			database: 'portal_noticias'
+		});
+
+		connection.query('select * from noticias', function(erro, result){
+			res.send(result);
+		});
+
+		//res.render("noticias/noticias")
 	});
 	app.get('/noticia', function(req, res){
 		res.render("noticias/noticia")
