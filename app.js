7817f226a55418065108bea828159baff33b3864
Marcos Rachid <marcosrachid@gmail.com>
2017-10-05 23:48:19 -0300

    externalizando configurações em modulo de config

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-var express = require('express');
-var msg = require('./mod_teste');
-
-var app = express();
-
-app.set('view engine', 'ejs');
+var app = require('./config/server');
 
 app.get('/', function(req, res){
 	res.render("home/index")
@@ -19,5 +14,5 @@ app.get('/noticia', function(req, res){
 });
 
 app.listen(3000, function(){
-	console.log(msg());
+	console.log("Running...");
 });
