86c418accf9cd7cb02b93ea24ae302d6367c92d3
Marcos Rachid <marcosrachid@gmail.com>
2017-10-06 23:50:45 -0300

    Criação de model para extração de dados do banco e isolamento de repositorio

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -1,11 +1,14 @@
 module.exports = function(app){
 
 	app.get('/noticia', function(req, res){
-		var connection = app.config.dbConnection();
 
-		connection.query('select * from noticias where id_noticia = 2', function(erro, result){
+		var connection = app.config.dbConnection();
+		var noticiasModel = app.app.models.noticiasModel;
+
+		noticiasModel.getNoticia(connection, function(erro, result){
 			res.render("noticias/noticia", {noticia: result});
 		});
+		
 	});
 
 }
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -5,10 +5,12 @@ module.exports = function(app){
 	app.get('/noticias', function(req, res){
 
 		var connection = app.config.dbConnection();
+		var noticiasModel = app.app.models.noticiasModel;
 
-		connection.query('select * from noticias', function(erro, result){
+		noticiasModel.getNoticias(connection, function(erro, result){
 			res.render("noticias/noticias", {noticias: result});
 		});
+		
 	});
 
 }
