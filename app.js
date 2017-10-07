12f889559299360755b13ae0315effc0f94640d1
Marcos Rachid <marcosrachid@gmail.com>
2017-10-06 23:16:02 -0300

    configurando rotas com consign

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 var app = require('./config/server');
-require('./app/routes/noticias')(app);
-require('./app/routes/formulario_inclusao_noticia')(app);
-require('./app/routes/home')(app);
 
 app.listen(3000, function(){
 	console.log("Running...");
